b8c833fd9532d0ad54badfd36043909474e11730
Rakesh Singh <rakeshsingh4811@gmail.com>
2026-06-07 19:44:28 +0530

    First Upload

--- script.js
+++ script.js
@@ -1,8 +1,1 @@
-const header = document.querySelector(".site-header");
-const menuToggle = document.querySelector(".menu-toggle");
-
-if (menuToggle && header) {
-  menuToggle.addEventListener("click", () => {
-    header.classList.toggle("is-open");
-  });
-}
+// The restored first design is intentionally static.
